399d9f231c213e1206739101c39e6c47dd7c4d7c
Ярослав Серый <goodyar@yandex.ru>
2016-06-21 16:35:03 +0300

    Первый вариант транспорта

--- transport/rabbitmq.py
+++ transport/rabbitmq.py
@@ -1,4 +1,28 @@
+import pika
+
+
 class RabbitMQ:
 
-    __init__(self):
-        pass
+    def __init__(self):
+        self.parameters = pika.ConnectionParameters()
+        self.message = Message()
+        self.listen_queue = Queue()
+        self.route_queue = Queue()
+
+    def connect(self):
+        self.connection = pika.BlockingConnection(self.parameters)
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(**self.listen_queue.declare_params())
+
+    def disconnect(self):
+        self.channel.close()
+        self.connection.close()
+
+    def reconnect(self):
+        if self.connection.is_closed(): self.connect()
+
+    def receive_sequence(self):
+        message = self.channel.consume(**self.listen_queue.consume_params())
+        self.channel.cancel()
+        self.message.from_list(message)
+        return self.message.body()
