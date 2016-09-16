b7bc6735fbe30cf7a49528c92ddb27746d02cb2f
Ярослав Серый <goodyar@yandex.ru>
2016-09-16 17:51:12 +0300

    Новый тип Result

---
+++ data/result.py
@@ -0,0 +1,25 @@
+import json
+
+
+class Result:
+
+    def __init__(self):
+        self.task_id = None
+        self.seq_id = None
+        self.return_code = None
+        self.return_value = None
+        self.error_text = None
+
+    def from_string(self, string):
+        result = json.loads(string)
+
+        self.task_id = result['task_id']
+        self.seq_id = result['seq_id']
+        self.return_code = result['return_code']
+
+        if 'return_value' in result:
+            self.return_value = result['return_value']
+        if 'error_text' in result:
+            self.error_text = result['error_text']
+
+        return self
